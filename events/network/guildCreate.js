fcaabecc7331bd0382e218e6ffff51fa2ff220fc
AroLeaf <starleafofa@gmail.com>
2022-11-04 07:59:35 +0100

    [refactor] single log statement

--- events/network/guildCreate.js
+++ events/network/guildCreate.js
@@ -4,6 +4,5 @@ export default new Event({
   event: Events.GuildCreate,
 }, async guild => {
   await guild.members.fetch();
-  console.log(`cached ${guild.members.cache.size} members for ${guild.name}`);
-  console.log(`total cached users: ${guild.client.users.cache.size}`);
+  console.log(`cached ${guild.members.cache.size} members for ${guild.name}\ntotal cached users: ${guild.client.users.cache.size}`);
 })
