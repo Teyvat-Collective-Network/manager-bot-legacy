817b19affa0202660ef5c22cee2e6525fc8708a9
AroLeaf <starleafofa@gmail.com>
2023-07-03 10:32:30 +0200

    [fix] stupid mistake

--- commands/network/partner/remove.js
+++ commands/network/partner/remove.js
@@ -20,7 +20,7 @@ parent.subcommand({
   if (!apiData.guild) return reply('This guild does not seem to be part of the TCN.');
   if (!(apiData.observer || apiData.owner)) return reply('A TCN server may only be removed from the network by its owner or a TCN observer.');
 
-  const success = await interaction.client.tcn.deleteGuild(apiData.guild.id).catch(() => {});
+  const success = await interaction.client.tcn.removeGuild(apiData.guild.id).catch(() => {});
 
   if (!success) return reply(`Failed to remove ${apiData.guild.name} from the network.`);
   await reply(`Successfully removed ${apiData.guild.name} from the network.`);
